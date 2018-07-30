01a57fcc1c0e7c01244203f966ae04671bfcf171
zgurya <a.zgurya@gmail.com>
2018-07-30 14:40:10 +0300

    Add remove plugin options

--- unistall.php
+++ unistall.php
@@ -5,4 +5,10 @@ if(!session_id()) {
 if(isset($_SESSION['wp_products_wg_target'])){
     unset($_SESSION['wp_products_wg_target']);
 }
+if(get_option('default-target-group')){
+    delete_option('default-target-group');
+}
+if(get_option('created-default-content')){
+    delete_option('created-default-content');
+}
 ?>
